3edb79ae4261b851727fd3b1772ad3ae17da1386
Ryan <ryantupo98@gmail.com>
2021-06-07 00:27:57 +0100

    06.06.2021 scraper and db all set up and working unique

--- secondscrape/database/db_connection.py
+++ secondscrape/database/db_connection.py
@@ -12,23 +12,29 @@ db = mysql.connector.connect(
 mycursor = db.cursor()
 
 def check_url(url):
-    print ("sdinhfsfjsdnbfljnsbdljfnbsdjolfnbsdojnbfsdnfosdjnbfojsdbnfojksdnbfojklsdnfljk")
     mycursor.execute(f"SELECT COUNT(1) FROM images WHERE link ='{url}'")
-    amount_of_matches = mycursor.fetchall()
     
-    print ("the amount of matches ????" , str(amount_of_matches))
-    print ("the amount of matches ????" , len(amount_of_matches))
+    amount_of_matches = mycursor.fetchall()
     
     list_of_matches = amount_of_matches[0]
     
+    if (list_of_matches[0]) > 0:
+        return True
+    else:
+        return False
+
+def check_link(url):
+    mycursor.execute(f"SELECT COUNT(1) FROM links WHERE link ='{url}'")
+    
+    amount_of_matches = mycursor.fetchall()
+    
+    list_of_matches = amount_of_matches[0]
     
     if (list_of_matches[0]) > 0:
         return True
     else:
         return False
     
-
-
 def commit_data():
     db.commit()
     print(mycursor.rowcount, "record inserted.")
@@ -39,17 +45,43 @@ def add_image(Link):
         mycursor.execute(f"INSERT INTO Images (link) VALUES ('{Link}')")
         commit_data()
     else:
-        print ("duplicate")    
+        print ("duplicate")   
+        
+def add_link(Link):
+    if check_link(Link) == False:
+        print ("WE GOT A LINK BABY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        mycursor.execute(f"INSERT INTO links (link) VALUES ('{Link}')")
+        commit_data()
+    else:
+        print ("duplicate")   
+         
 def print_table():
     mycursor.execute("SELECT * FROM Images")
     table = mycursor.fetchall()
     for i in table:
         print (i)
 
+def add_row():
+     mycursor.execute("CREATE TABLE links (link VARCHAR(244), linkID int primary key AUTO_INCREMENT)")
+    
+def log_data(html):
+    with open("frontpage.html", "w") as page:
+        page.write(html)
+        page.close()
+
+
+    with open("frontpage.json", "w") as page:
+        page.write(html)
+        page.close()
+
+
+
+
+
 # add_image("https://images-na.ssl-images-amazon.com/Mobile_IT._CR0,240,3000,720_SX1250_.jpg")
 
 
-
+# mycursor.execute("CREATE TABLE links (link VARCHAR(244), linkID int primary key AUTO_INCREMENT)")
 #mycursor.execute("CREATE TABLE Images (link VARCHAR(244), imageID int primary key AUTO_INCREMENT)")
 # mycursor.execute("CREATE DATABASE katsdb")
 
--- secondscrape/spiders/kats.py
+++ secondscrape/spiders/kats.py
@@ -3,51 +3,51 @@ import mysql.connector
 from secondscrape.database.db_connection import *;
 
 
-
-
 class kats(scrapy.Spider):
-    
 
-    
     name = "kats"
     start_urls = ["https://www.pinterest.co.uk/joyishkennedy4/kit-kat-pics/"]
 
+
+        
+
+
     def parse(self, response):
+        
+        
         image_links = []
         links = response.xpath("//img/@src")
+        url_links = response.xpath("//a/@href")
         html = ""
+        
+        for url_link in url_links:
+            url = link.get()
+            
+            if any(extension in url for extension in [".html", ".php"]):
+                
+                add_link(url)
 
         for link in links:
             url = link.get()
 
             if any(extension in url for extension in [".jpg", ".gif", ".png"]):
-            
 
                 html += """<a href="{url}"
                 target="_blank"><img src="{url}"
                 height="33%"
                 width="33%"/><a/>""".format(url=url)
-                
-                print ("sdfdsenbflsdfhnbsdkjfnsdopjnfsjdknfolpjksdnfjksdslondfnsdk;nfs;dknfksd")
+
 
                 add_image(url)
                 
-                
 
-                with open("frontpage.html", "a") as page:
-                    page.write(html)
-                    page.close()
-                
+                log_data(html)
 
-                with open("frontpage.json", "a") as page:
-                    page.write(html)
-                    page.close()
-                
                 image_links.append("{url}")
 
-                
-            
-            next_page = response.css('li.next a::attr(href)').get()
-            if next_page is not None:
-                next_page = response.urljoin(next_page)
-                yield scrapy.Request(next_page, callback=self.parse)
+            # next_page = response.css('li.next a::attr(href)').get()
+            # if next_page is not None:
+            #     next_page = response.urljoin(next_page)
+            #     yield scrapy.Request(next_page, callback=self.parse)
+
+
